15f5704403bec49404c475876e500b03c10bd821
saWoW <swpants05@gmail.com>
2025-07-10 17:34:44 +0900

    주피터 노트북 모음 브랜치 따로 만듦

--- resume_main.sh
+++
@@ -1,9 +0,0 @@
-# python3 resume_main.py \
-#   --checkpoint result/varnet_small/checkpoints/model.pt \
-#   --override_lr 5e-4 \
-#   --override_epochs 50
-
-  python3 resume_main.py \
-  --checkpoint  /home/introai7/.bin/.fmri/result/small_varnet_resume_test/checkpoints/model.pt  # 긱지 환경에 맞게 변경해야 함!
-
-  --override_epochs 30
