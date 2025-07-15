f643ca7d522cd1d9e1fe3b2f7f732b74865a7d57
saWoW <swpants05@gmail.com>
2025-07-15 17:42:02 +0900

    mask augmenter 오류 디버깅 완료

--- utils/augmentations/maskaugmenter.py
+++ utils/augmentations/maskaugmenter.py
@@ -99,6 +99,7 @@ class MaskAugmenter:
         # ② 파라미터 샘플링
         accel = self._sample_param(spec["accel"])
         cf    = self._sample_param(spec["cf"])
+        print(mtype, accel, cf)
         mf = create_mask_for_mask_type(
             mtype,
             center_fractions=[cf],
@@ -110,9 +111,82 @@ class MaskAugmenter:
         return mask.numpy()
 
     # -------------------------- core -------------------------------------
-    def __call__(self, mask, kspace_np, target_np, attrs, fname, slice_idx, *extra):
+    # def __call__(self, mask, kspace_np, target_np, attrs, fname, slice_idx, *extra):
+    #     if self.rng.rand() >= self._prob_now():
+    #         return mask, kspace_np, target_np, attrs, fname, slice_idx, *extra
+    #     # 새 mask 생성 (shape = kspace[..., H, W])
+    #     new_mask = self._make_mask(kspace_np.shape)
+    #     print(mask.shape, new_mask.shape, kspace_np.shape)
+    #     return new_mask, kspace_np, target_np, attrs, fname, slice_idx, *extra
+
+    # def __call__(self, mask, kspace_np, target_np, attrs, fname, slice_idx, *extra):
+
+    #     # 1) 확률 밖이면 원본 mask 그대로
+    #     if self.rng.rand() >= self._prob_now():
+    #         return mask, kspace_np, target_np, attrs, fname, slice_idx, *extra
+
+    #     # 2) 원래 mask 길이 W
+    #     W = mask.shape[0]
+
+    #     # 3) MaskFunc 에 넘길 fake_shape: shape[-2] == W 가 되도록 (1, W, 1)
+    #     fake_shape = (1, W, 1)
+
+    #     # 4) _make_mask(fake_shape) → numpy array of shape (1, W, 1)
+    #     new_mask_raw = self._make_mask(fake_shape)
+
+    #     # 5) squeeze & reshape to original 1D
+    #     #    (1, W, 1) -> (W,)
+    #     new_mask = new_mask_raw.squeeze().reshape(mask.shape)
+
+    #     # 6) 리턴
+    #     return new_mask, kspace_np, target_np, attrs, fname, slice_idx, *extra
+
+    def __call__(self, mask, kspace_np, target_np, attrs, fname, slice_idx):
+        # 1) 확률 밖이면 원본 mask 그대로
         if self.rng.rand() >= self._prob_now():
-            return mask, kspace_np, target_np, attrs, fname, slice_idx, *extra
-        # 새 mask 생성 (shape = kspace[..., H, W])
-        new_mask = self._make_mask(kspace_np.shape)
-        return new_mask, kspace_np, target_np, attrs, fname, slice_idx, *extra
+            return mask, kspace_np, target_np, attrs, fname, slice_idx
+
+        # 2) cat 문자열에서 acceleration 추출: "_x4" 이면 4, 아니면 8
+        cat = attrs.get('cat', '')
+        curr_acc = 4 if cat.endswith("_x4") else 8
+
+        # 3) mask type 하나 선택 후, 이 acc가 허용되지 않으면 스킵
+        mtype = self.rng.choice(self.types, p=self.type_probs)
+        allowed_accel = self.mask_specs[mtype].get("accel", [])
+        if curr_acc not in allowed_accel:
+            return mask, kspace_np, target_np, attrs, fname, slice_idx
+
+        # 4) CF만 uniform 샘플링 (list or range 둘 다 지원)
+        cf_spec = self.mask_specs[mtype]["cf"]
+        if isinstance(cf_spec, Sequence) and len(cf_spec) == 2:
+            cf = self.rng.uniform(cf_spec[0], cf_spec[1])
+        elif isinstance(cf_spec, Sequence):
+            cf = self.rng.choice(cf_spec)
+        else:
+            cf = cf_spec
+
+        # 5) fake_shape 준비: MaskFunc가 shape[-2]를 num_cols로 사용하므로 (1, W, 1)
+        W = mask.shape[0]
+        fake_shape = (1, W, 1)
+
+        # 6) MaskFunc 생성 & 호출
+        mf = create_mask_for_mask_type(
+            mtype,
+            center_fractions=[cf],
+            accelerations=[curr_acc],
+        )
+        if hasattr(mf, "allow_any_combination"):
+            mf.allow_any_combination = self.allow_any
+        # 반환값은 (mask_tensor, num_low_freq); tensor → numpy
+        new_mask_raw, _ = mf(fake_shape)
+        new_np = new_mask_raw.numpy()
+
+        # 7) 원본 mask와 똑같은 1D shape 으로 복원
+        new_mask = new_np.squeeze().reshape(mask.shape)
+
+        # (디버그) 확인하고 싶으면 찍어보세요
+        # print("self.types", self.types, "self.type_probs", self.type_probs)        
+        # print("mtype:", mtype, "cf:", cf, "curr_acc:", curr_acc,"cat:", cat )
+        # print("orig:", mask.shape, "new:", new_mask.shape)
+        # print(new_mask)
+        return new_mask, kspace_np, target_np, attrs, fname, slice_idx
